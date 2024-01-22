0776218fc184d3d81c0161d33d229e1a557838c8
GQH123 <572694553@qq.com>
2024-01-23 02:06:03 +0800

    [mv] move deprecated RFC out of workspace

--- RFC_deprecated/RFC/itemset/RawItemsetConfig.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass, field
-
-from .ItemsetConfig import ItemsetConfig
-
-
-@dataclass
-class RawItemsetConfig(ItemsetConfig):
-    ...
--- RFC_deprecated/RFC/itemset/__init__.py
+++
@@ -1,1 +0,0 @@
-../utils/structural_initializer.py
