e4f0136c3822d1e0cd89e38ef968130f3d94976f
GQH123 <572694553@qq.com>
2024-01-30 13:20:05 +0800

    [rm] remove workdir (for now)

--- workdir/hitomi.la/authors/crawler.py
+++ workdir/hitomi.la/authors/crawler.py
@@ -1,32 +1,38 @@
-from RFC.args.arg_group import *
-from RFC.item.item import *
+from RFC.item.item import ItemType
 
 class HitomiPage(ItemType):
-    _defined_arg_groups = {
-        RequestArgGroup(
-            url=('fixed', 'https://hitomi.la/artist/mutou%20mato-all.html'),
-            params=('replace_id', 'page={id}'),
-        ),
-        ItemArgGroup(
-            save_dir=('auto', 'saves', 'subs'),
-        ),
+    _name: str = 'itemtype_hitomi_page'
+
+    _logger = None
+    request_arg_group = {
+        'url': ('replace_id', 'https://hitomi.la/artist/mutou%20mato-all.html?page={id}'),
+        'proxies': ('fixed', {
+            'http': 'http://127.0.0.1:7890',
+            'https': 'http://127.0.0.1:7890',
+        }),
     }
-    session_args = SessionArgs(
-        no_session=False,
-        lib='requests',
-    )
+    item_arg_group = {
+        'save_dir': ('auto', 'saves', 'subs'),
+    }
+    session_args = {
+        'no_session': False,
+        'lib': 'requests',
+    }
     middleware_args = {
-        'status_code': MiddlewareArgs(
-            expected_status_codes=[200],
-        ),
-        'basic': MiddlewareArgs(),
-        'saver': MiddlewareArgs(),
+        'status_code': {
+            'expected_status_codes': [200],
+        },
+        'basic': {},
+        'saver': {},
     }
-    requestor_args = RequestorArgs(
-        nproc=1,
-        async_sema=1,
-    )
+    requestor_args = {
+        'nproc': 5,
+        'async_sema': 1,
+    }
+    
+    @classmethod
     def _generate(cls, id, result):
         pass
 
 
+HitomiPage.start(range(3), debug_n=3)
