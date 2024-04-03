71010c64d63c4faeffc338a7d9fe68249bdc8afb
GQH123 <572694553@qq.com>
2024-04-04 01:53:03 +0800

    [upd] enable ProxyAPI logger

--- RFC/utils/defs.py
+++ RFC/utils/defs.py
@@ -17,7 +17,7 @@ RotatingFileHandler_config = dict(
 
 ArgGroup_logger_enable_file_handler = False
 Requestor_logger_enable_file_handler = True
-ProxyAPI_logger_enable_file_handler = False
+ProxyAPI_logger_enable_file_handler = True
 RFC_logger_enable_file_handler = False
 
 # Visit Status of ArgSetters
