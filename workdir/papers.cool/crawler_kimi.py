39ddfb2cc142df34d79b86eccdface63493d5db4
GQH123 <572694553@qq.com>
2024-04-08 17:53:31 +0800

    [upd] add notes, lazy inited cookies, distributed proxies/process-wise proxies, datewise arxiv paper index, reduced nproc, switch request lib from aiohttp to requests,

--- workdir/papers.cool/crawler_kimi.py
+++ workdir/papers.cool/crawler_kimi.py
@@ -5,26 +5,24 @@ import time
 import random
 import requests
 
+
 from RFC.item.item import ItemType
 from RFC.utils.parse import (
     get_html_soup,
     parse,
 )
 
-
-# default_proxy_config = {
-#     'http': 'http://127.0.0.1:7890',
-#     'https': 'http://127.0.0.1:7890',
-#     'all': 'socks5://127.0.0.1:7890',
-# }
-
-
 default_proxy_config = {
-    'http': 'http://10.176.52.116:7890',
-    'https': 'http://10.176.52.116:7890',
-    'all': 'socks5://10.176.52.116:7890',
+    'http': 'http://127.0.0.1:7890',
+    'https': 'http://127.0.0.1:7890',
+    'all': 'socks5://127.0.0.1:7890',
 }
 
+# default_proxy_config = {
+#     'http': 'http://10.176.52.116:7890',
+#     'https': 'http://10.176.52.116:7890',
+#     'all': 'socks5://10.176.52.116:7890',
+# }
 
 # manually maintained venus tags
 # https://papers.cool/venue/{venue_tag}?show=200000
@@ -42,28 +40,37 @@ venue_tags += [f'ICML.{year}' for year in range(2021, 2023+1)]
 venue_tags += [f'ICLR.{year}' for year in range(2021, 2024+1)]
 
 
-# manually maintained arxiv tags
-# https://papers.cool/arxiv/{arxiv_tag}?show=200000
-arxiv_tags = [
-    'cs.AI',
-    'cs.CV',
-    'cs.CL',
-    'cs.LG',
-]
+def get_full_date_tags(st_year=2024, st_month=1, st_day=1):
+    from datetime import date, timedelta
 
+    def daterange(start_date, end_date):
+        for n in range(int((end_date - start_date).days)):
+            yield start_date + timedelta(n)
+
+    start_date = date(st_year, st_month, st_day)
+    end_date = date.today() + timedelta(1)
+    full_dates = []
+    for single_date in daterange(start_date, end_date):
+        full_dates.append(single_date.strftime("%Y-%m-%d"))
+    return full_dates
+
+
+arxiv_date_tags = get_full_date_tags()
+
+# manually maintained arxiv tags
+# https://papers.cool/arxiv/cs.AI?date=2024-04-02&show=200000
+# https://papers.cool/arxiv/{arxiv_tag}&show=200000
+arxiv_tags = []
+arxiv_tags += [f'cs.{tag}?date={date_tag}' for tag in ['AI', 'CV', 'CL', 'LG'] for date_tag in arxiv_date_tags]
 
 type_tag_dict = {
     'venue': venue_tags,
     'arxiv': arxiv_tags,
 }
 
-
 _test_fixed_cookies = {
     'client_id': "!P3n0PpTkvLxE77zUkxZw6Q==?gAWVKgAAAAAAAACMCWNsaWVudF9pZJSMGDM3MDAzLTE3MTE5NjE1NzguNjY3NDIzMpSGlC4=",
 }
-
-
-cookies_pool = json.load(open('cookies.json'))
 
 
 class PapersCoolPaperPage(ItemType):
@@ -71,7 +78,7 @@ class PapersCoolPaperPage(ItemType):
 
     _logger = None
     request_arg_group = {
-        'url': ('field', 'https://papers.cool/{paper_type}/{paper_tag}?show=200000'),
+        'url': ('field', 'https://papers.cool/{paper_type}/{paper_tag}{symbol}show=200000'),  # symbol is '?' if paper_type is `venue` else '&', item id will not contain this symbol
         'proxies': ('fixed', default_proxy_config),
         # 'cookies': ('field', '{cookies}'),
         'cookies': ('fixed', _test_fixed_cookies),
@@ -82,7 +89,7 @@ class PapersCoolPaperPage(ItemType):
     }
     session_args = {
         'no_session': False,
-        'lib': 'aiohttp',
+        'lib': 'requests',
     }
     middleware_args = {
         'status_code': {
@@ -93,11 +100,11 @@ class PapersCoolPaperPage(ItemType):
         'result_saver': {},
     }
     requestor_args = {
-        'nproc': 12,
-        'async_sema': 24,
+        'nproc': 3,
+        'async_sema': 1,
         'wait_timeout': 20,
         'wait_sleep': 1,
-        'report_step': 10,
+        'report_step': 1,
     }
     
     @classmethod
@@ -197,22 +204,82 @@ class PapersCoolPaperPage(ItemType):
 #         json.dump(cookies, open(os.path.join(item.save_dir, 'cookies.json'), 'w'), indent=4)
 
 
-def random_choose_cookies(*args, **kwargs):
-    return {'client_id': random.choice(cookies_pool)}
+# cookies_pool = json.load(open('cookies.json'))
+# def random_choose_cookies(*args, **kwargs):
+#     return {'client_id': random.choice(cookies_pool)}
+
+
+def lazy_request_cookies_online(*args, **kwargs):
+    proxies = kwargs.pop('proxies', default_proxy_config)
+    for i in range(5):
+        try:
+            resp = requests.request('post', "https://papers.cool/venue/star?key=kimi&paper=P16-1019@ACL", proxies=proxies)
+            return dict(resp.cookies)
+        except Exception as e:
+            error_report = f'cookies request error: [{type(e).__name__}] {e}'
+            print(error_report, flush=True)
+            time.sleep(0.5)
+            continue
+    error_report = 'cookies request error: all retries failed'
+    print(error_report, flush=True)
+    return {}
+
+
+proxy_pool = (
+    {
+        'http': 'http://127.0.0.1:10000',
+        'https': 'http://127.0.0.1:10000',
+        'all': 'socks5://127.0.0.1:10000',
+    },
+    {
+        'http': 'http://127.0.0.1:10002',
+        'https': 'http://127.0.0.1:10002',
+        'all': 'socks5://127.0.0.1:10002',
+    },
+    {
+        'http': 'http://127.0.0.1:10004',
+        'https': 'http://127.0.0.1:10004',
+        'all': 'socks5://127.0.0.1:10004',
+    },
+)
 
 
 class PapersCoolPaperKimiSection(PapersCoolPaperPage):
+    """
+        Notes:
+            - `timeout` 
+                - If you do not want kimi to return the response, set timeout to some value but not `None`, this will click the kimi generation buttons and make the queue blocked if too fast. The admin of papers.cool can easily detect this and restart the website.
+                - Otherwise, set this to `None`, this will make your crawler to get the response and more invisible.
+            - `proxies`
+                - Now the admin of papers.cool has prohibited the frequent access to the website from the same IP address, so you need to use proxies to avoid being blocked.
+                - QGNet API is pointless if you want to get the response. It is only useful when you just want to click the kimi generation buttons and block the queue.
+                - Proxy Pool can be casually specified, as long as it is durable. Then you can use the proxy one per process and set `timeout` to `None`.
+                    - The admin of papers.cool will not detect frequency access to the already generated kimi sections, but only block your frequent click to the kimi generation buttons. So it's safe to use a durable proxy.
+                    - To launch multiple local proxy servers, you can refer to `./dist_clash` directory.
+            - `cookies`
+                - Cookies will be invalid after a while (approximately 12 hours). You can execute the `extract_cookies.py` to renew the cookies.
+                    - If invalid cookies are used, the website will return html contents of `无效访问` but still with status code 200. If you see this, you need to execute `remove_subs.py` first to clear such seemingly successfully fetched items.
+                    - If `extract_cookies.py` cannot get the cookies, you need to check if the cookies-extracting link (in the script) is still valid.
+            - `lib`
+                - It seems that the admin of papers.cool has blocked the `aiohttp` library but not `requests`. So you should use the latter. In fact async requests is pointless here after the frequency of access is prohibited.
+    """
     _name: str = 'papers_cool_paper_kimi_section'
 
     _logger = None
     request_arg_group = {
         'method': ('fixed', 'post'),
         'url': ('field', '{kimi_link}'),
-        'proxies': ('fixed', default_proxy_config),
-        'cookies': (random_choose_cookies,),
+        # 'proxies': ('fixed', default_proxy_config),
+        # 'proxies': ('qgnet', 'D6FL1CJ8', '9FC1ADB88090'),
+        'proxies': ('pool', proxy_pool),
+        # 'cookies': (random_choose_cookies,),
+        'cookies': ('lazy_func', lazy_request_cookies_online),
         # 'cookies': ('fixed', _test_fixed_cookies),  # 访问过快，请11360.14秒后重试(
         'is_leaf': ('fixed', True),
+        # 'timeout': ('fixed', None),
         'timeout': ('fixed', None),
+        'user_agent': ('random',),
+        'referer': ('host',),
     }
     
     @classmethod
@@ -221,7 +288,7 @@ class PapersCoolPaperKimiSection(PapersCoolPaperPage):
 
 
 ids = [f'{type}_{tag}' for type in type_tag_dict for tag in type_tag_dict[type]]
-items_kwargs = [{'paper_type': type, 'paper_tag': tag} for type in type_tag_dict for tag in type_tag_dict[type]]
+items_kwargs = [{'paper_type': type, 'paper_tag': tag, 'symbol': '?' if type == 'venue' else '&'} for type in type_tag_dict for tag in type_tag_dict[type]]
 PapersCoolPaperPage._add_items(ids=ids, items_kwargs=items_kwargs, bloodline=[])
 PapersCoolPaperPage.start()
 
