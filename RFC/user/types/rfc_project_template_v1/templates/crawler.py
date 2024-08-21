83bfe2a2a39442abfa9fd22a66b96ed666ec8e88
GQH123 <572694553@qq.com>
2024-08-21 12:29:25 +0000

    [upd] update auto_skip function in template crawler.py

--- RFC/user/types/rfc_project_template_v1/templates/crawler.py
+++ RFC/user/types/rfc_project_template_v1/templates/crawler.py
@@ -62,28 +62,48 @@ class MyItemType(ItemType):
 
 
 if __name__ == '__main__':
-    auto_skipped_ids = {}
-    if os.path.exists('saved_logs/auto_skipped_ids.json'):
-        auto_skipped_ids = json.load(open('saved_logs/auto_skipped_ids.json', 'r'))
-    if os.path.exists('saved_logs/statistics_finished_status_details.json'):
-        parsed_skipped_ids = json.load(open('saved_logs/statistics_finished_status_details.json', 'r'))['__all__']['MyItemType']
-        for item_type in parsed_skipped_ids:
-            auto_skipped_ids[item_type] = list(set(auto_skipped_ids.get(item_type, []) + parsed_skipped_ids[item_type]))
-    auto_skipped_ids_count = {item_type: len(auto_skipped_ids[item_type]) for item_type in auto_skipped_ids}
-    print(f'count for auto_skipped_ids:\n{json.dumps(auto_skipped_ids_count, indent=4, ensure_ascii=False)}')
-    json.dump(auto_skipped_ids, open('saved_logs/auto_skipped_ids.json', 'w'), indent=4, ensure_ascii=False)
-    auto_skipped_ids = {item_type: set(auto_skipped_ids[item_type]) for item_type in auto_skipped_ids}
+    """ example for collecting skipped ids, you should run `python extract.py` first, you can also add other skipped ids by yourself
+        # get current skipped ids
+        auto_skipped_ids = {}
+        if os.path.exists('saved_logs/auto_skipped_ids.json'):
+            auto_skipped_ids = json.load(open('saved_logs/auto_skipped_ids.json', 'r'))
+        
+        # add skipped ids from finished items type-by-type
+        if os.path.exists('saved_logs/statistics_finished_status_details.json'):
+            parsed_skipped_ids = json.load(open('saved_logs/statistics_finished_status_details.json', 'r'))['__all__']
+            for item_type in parsed_skipped_ids:
+                auto_skipped_ids[item_type] = list(set(auto_skipped_ids.get(item_type, []) + parsed_skipped_ids[item_type]))
+                
+        # you can also add skipped ids from failed items, such as ones with status code 404 error
+        # if os.path.exists('saved_logs/statistics_failed_status_details.json'):
+        #     parsed_skipped_ids = json.load(open('saved_logs/statistics_failed_status_details.json', 'r'))['StatusCode 404 Error']['type']
+        #     for item_type in parsed_skipped_ids:
+        #         auto_skipped_ids[item_type] = list(set(auto_skipped_ids.get(item_type, []) + parsed_skipped_ids[item_type]))
+        
+        # get skipped id counts type-by-type
+        auto_skipped_ids_count = {item_type: len(auto_skipped_ids[item_type]) for item_type in auto_skipped_ids}
+        print(f'count for auto_skipped_ids:\n{json.dumps(auto_skipped_ids_count, indent=4, ensure_ascii=False)}')
+        
+        # save skipped ids, and convert list to set for better filter performance
+        json.dump(auto_skipped_ids, open('saved_logs/auto_skipped_ids.json', 'w'), indent=4, ensure_ascii=False)
+        auto_skipped_ids = {item_type: set(auto_skipped_ids[item_type]) for item_type in auto_skipped_ids}
+    """
 
     """ example for filtering ids
-    ids = [f'{type}_{tag}' for type in type_tag_dict for tag in type_tag_dict[type]]
-    items_kwargs = [{'paper_type': type, 'paper_tag': tag, 'symbol': '?' if type == 'venue' else '&'} for type in type_tag_dict for tag in type_tag_dict[type]]
-    print(f'count for all ids: {len(ids)}\n')
-    ids_items_kwargs = [(id, item_kwargs) for id, item_kwargs in zip(ids, items_kwargs) if id not in auto_skipped_ids.get('PapersCoolPaperPage', set())]
-    if ids_items_kwargs:
-        ids, items_kwargs = zip(*ids_items_kwargs)
-    else:
-        ids, items_kwargs = [], []
-    print(f'count for added ids: {len(ids)}\n')
+        # get all ids and items_kwargs
+        ids = get_all_ids()
+        items_kwargs = get_all_items_kwargs()
+        if items_kwargs is None:
+            tems_kwargs = [{} for id in ids]
+        print(f'count for all ids: {len(ids)}\n')
+        
+        # filter ids by item type
+        ids_items_kwargs = [(id, item_kwargs) for id, item_kwargs in zip(ids, items_kwargs) if str(id) not in auto_skipped_ids.get('MyItemType', set())]
+        if ids_items_kwargs:
+            ids, items_kwargs = zip(*ids_items_kwargs)
+        else:
+            ids, items_kwargs = [], []
+        print(f'count for added ids: {len(ids)}\n')
     """
 
     # make sure item id contains no '/' so that item.save_dir will be a valid path, and better use unique id for each item (but not necessary) for convenience
