9e23d3238c94e0e4ba906ca9b0b7104cfe13f608
GQH123 <572694553@qq.com>
2024-08-21 11:04:31 +0000

    [add] add setup.py for installing RFC as pip package

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import find_packages, setup
+
+setup(
+    name="RFC",
+    py_modules=["RFC"],
+    version="0.0.1",
+    description="Renatus' Fast Crawler",
+    author="Renatus",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "rfc = RFC.user.main:main",
+        ],
+    },
+    install_requires=[],
+    include_package_data=True,
+)
