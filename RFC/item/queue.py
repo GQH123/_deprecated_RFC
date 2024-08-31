837e273f313ab761a785aa7c8879b5c4f4219027
GQH123 <572694553@qq.com>
2024-08-31 15:46:59 +0000

    [upd] add prior queue for high priority items

--- RFC/item/queue.py
+++ RFC/item/queue.py
@@ -36,6 +36,7 @@ class RootQueue(RootType):
     def lazy_init(cls):  # lazy init for pickle error, must be called after any custom `ItemType` definition
         manager = get_global_manager()
         RootQueue._root_item_queue = manager.list()
+        RootQueue._prior_item_queue = manager.list()
         RootQueue._step = manager.Value('i', 0)
         RootQueue._active_item_count = manager.Value('i', 0)
         RootQueue._active_adder_count = manager.Value('i', 0)
@@ -46,20 +47,20 @@ class RootQueue(RootType):
         """
             Add a new item to `RootQueue`.
         """
-        # in case queue is too long, wait for a while
+        _added_queue = RootQueue._root_item_queue if not item._prior_queue else RootQueue._prior_item_queue
+        # in case queue is too long, wait for a while ~(or the last item block the queue, [deprecated, too slow])
         while True:
             wait_for_queue = False
             with get_global_lock():
-                if len(RootQueue._root_item_queue) >= 100000:
-                    wait_for_queue = True
+                queue_len = len(_added_queue)
+            wait_for_queue = (queue_len >= 100000)
             if wait_for_queue:
                 time.sleep(10)
             else:
                 break
-        
         with get_global_lock():
             RootQueue._step.value += 1
-            RootQueue._root_item_queue.append(item)  # type: ignore # now the item is of type `Item` but not `ItemType`
+            _added_queue.append(item)  # type: ignore # now the item is of type `Item` but not `ItemType`
             if RootQueue._step.value & 1023 == 0:
                 RootQueue._logger.info(f"{repr(item)} added")
                 # cls._logger.info(f"{RootQueue._step.value} items added")
@@ -68,23 +69,24 @@ class RootQueue(RootType):
     @classmethod
     def report(cls):
         while True:
-            time.sleep(RootQueue._report_wait_time)
             with get_global_lock():
-                if not RootQueue._root_item_queue and RootQueue._active_item_count.value == 0 and RootQueue._active_adder_count.value == 0:
+                if not RootQueue._root_item_queue and not RootQueue._prior_item_queue and RootQueue._active_item_count.value == 0 and RootQueue._active_adder_count.value == 0:
                     RootQueue._logger.info(f"all items finished, RootQueue will be closed")
-                    break
-                RootQueue._logger.info(f"{RootQueue._step.value} items added, qic = {len(RootQueue._root_item_queue)}, aic = {RootQueue._active_item_count.value}, aac = {RootQueue._active_adder_count.value}")
+                    return
+                RootQueue._logger.info(f"{RootQueue._step.value} items added, qic = {len(RootQueue._root_item_queue)}, prqic = {len(RootQueue._prior_item_queue)}, aic = {RootQueue._active_item_count.value}, aac = {RootQueue._active_adder_count.value}")
+            time.sleep(RootQueue._report_wait_time)
 
     @classmethod
     def fetch(cls):  # cls must be RootQueue in this case
         with get_global_lock():
-            if not RootQueue._root_item_queue:
+            _fetched_queue = RootQueue._prior_item_queue if RootQueue._prior_item_queue else RootQueue._root_item_queue
+            if not _fetched_queue:  # it is impossible that prior queue is empty now
                 # assert RootQueue._active_item_count.value >= 0 and RootQueue._active_adder_count.value >= 0, f"error occurs in RootQueue, remain_active_items={RootQueue._active_item_count.value}, remain_active_adders={RootQueue._active_adder_count.value}"
                 if RootQueue._active_item_count.value == 0 and RootQueue._active_adder_count.value == 0:
                     return 'QUIT'
                 else:
                     return 'WAIT'
-            item = RootQueue._root_item_queue.pop()
+            item = _fetched_queue.pop()
             RootQueue._active_item_count.value += 1  # this will be reduced in item.finish()
         item.pend()
         # RootQueue._logger.info(f"{repr(item)} fetched")  
